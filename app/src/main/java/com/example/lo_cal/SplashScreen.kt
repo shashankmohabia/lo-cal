0b4cedb438f2936b412131b2508e2482a50f4598
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-04 16:15:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lo_cal/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.lo_cal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
