19f73f639272994fa4e295b76f86c40483505fcf
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-04 17:32:59 +0530

    Add dataBinding to GameFragment and ResultFragment

--- app/src/main/java/com/example/lo_cal/GameFragment.kt
+++ app/src/main/java/com/example/lo_cal/GameFragment.kt
@@ -5,18 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.lo_cal.databinding.FragmentGameBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class GameFragment : Fragment() {
 
+    lateinit var binding: FragmentGameBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+        return binding.root
     }
 
 }
