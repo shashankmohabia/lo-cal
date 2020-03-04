06e4ded2c52a3f9ca01dac3d2fd1acd0c9645980
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-04 18:26:51 +0530

    Update result textView with arguments in ResultFragment

--- app/src/main/java/com/example/lo_cal/ResultFragment.kt
+++ app/src/main/java/com/example/lo_cal/ResultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
 import com.example.lo_cal.databinding.FragmentGameBinding
@@ -16,12 +17,23 @@ import com.example.lo_cal.databinding.FragmentResultBinding
 class ResultFragment : Fragment() {
 
     lateinit var binding: FragmentResultBinding
+    private lateinit var args: ResultFragmentArgs
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        args = ResultFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(activity, arguments.toString(), Toast.LENGTH_LONG).show()
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.result.text = args.firstPersonName
+
     }
 
 }
