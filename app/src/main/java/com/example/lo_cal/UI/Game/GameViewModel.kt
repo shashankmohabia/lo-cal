43d9c453c80764ab13a16b5dc5abc6e02c6e951c
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-05 18:50:42 +0530

    Add result field in GameFragment | Add result calculation function

--- app/src/main/java/com/example/lo_cal/UI/Game/GameViewModel.kt
+++ app/src/main/java/com/example/lo_cal/UI/Game/GameViewModel.kt
@@ -1,6 +1,18 @@
 package com.example.lo_cal.UI.Game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    val result = MutableLiveData<Int>()
+
+    init {
+        result.value = 0
+    }
+
+    fun getResult() {
+        result.value = (1..100).random()
+    }
+
 }
