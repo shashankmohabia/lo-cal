e60329c5de8f38c2717ca522ee1cf675f93f4977
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-12 15:24:45 +0530

    Shift AboutFragment to ui package

--- app/src/main/java/com/example/lo_cal/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/lo_cal/ui/about/AboutFragment.kt
@@ -1,10 +1,11 @@
-package com.example.lo_cal
+package com.example.lo_cal.ui.about
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.lo_cal.R
 
 /**
  * A simple [Fragment] subclass.
