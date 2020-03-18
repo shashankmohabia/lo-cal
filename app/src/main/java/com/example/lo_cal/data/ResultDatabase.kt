58d5395b4b21f9af16c4e59adb194ff22ffea616
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-19 01:41:41 +0530

    Create ResultDatabase

--- app/src/main/java/com/example/lo_cal/data/ResultDatabase.kt
+++ app/src/main/java/com/example/lo_cal/data/ResultDatabase.kt
@@ -5,26 +5,30 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.lo_cal.data.models.LoCalEntry
-import com.example.lo_cal.data.Database as DB
+import com.example.lo_cal.utils.constants.DATABASE_NAME
 
 @Database(entities = [LoCalEntry::class], version = 1, exportSchema = false)
-abstract class Database : RoomDatabase() {
+abstract class ResultDatabase : RoomDatabase() {
 
     abstract val dao: DBDao
 
     companion object {
-        private var INSTANCE: DB? = null
+        @Volatile
+        private var INSTANCE: ResultDatabase? = null
 
-        fun getInstance(context: Context): DB {
+        fun getInstance(context: Context): ResultDatabase {
             synchronized(this) {
                 var instance = INSTANCE
-                if(instance==null){
+                if (instance == null) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
-                        com.example.lo_cal.data.Database::class,
-                        
-
+                        ResultDatabase::class.java,
+                        DATABASE_NAME
                     )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
                 }
                 return instance
             }
--- app/src/main/java/com/example/lo_cal/utils/constants/DataBaseConstants.kt
+++ app/src/main/java/com/example/lo_cal/utils/constants/DataBaseConstants.kt
@@ -1,3 +1,4 @@
 package com.example.lo_cal.utils.constants
 
 const val DATABASE_TABLE_NAME = "database_table"
+const val DATABASE_NAME = "result_database"
