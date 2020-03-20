6f90dbb4d169aa83be781c5bed5465a64c8fa0b2
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-20 12:53:01 +0530

    Add a job to ResultViewModel

--- app/src/main/java/com/example/lo_cal/ui/result/ResultViewModel.kt
+++ app/src/main/java/com/example/lo_cal/ui/result/ResultViewModel.kt
@@ -4,12 +4,15 @@ import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
 
 class ResultViewModel(val args: ResultFragmentArgs, val application: Application) : ViewModel() {
 
     private val _calculateAgain = MutableLiveData<Boolean>()
     val calculateAgain: LiveData<Boolean>
         get() = _calculateAgain
+
+    private var viewModelJob = Job()
 
     init {
         _calculateAgain.value = false
@@ -23,5 +26,8 @@ class ResultViewModel(val args: ResultFragmentArgs, val application: Application
         _calculateAgain.value = false
     }
 
-
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
