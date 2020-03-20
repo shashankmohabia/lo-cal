fa996da74bc3749ec16ea3c5fafee912368c25cd
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-20 16:07:06 +0530

    Update return type of get query

--- app/src/main/java/com/example/lo_cal/data/DBDao.kt
+++ app/src/main/java/com/example/lo_cal/data/DBDao.kt
@@ -18,7 +18,7 @@ interface DBDao {
     fun update(entry: LoCalEntry)
 
     @Query("SELECT * FROM $DATABASE_TABLE_NAME WHERE id = :key")
-    fun get(key: String): LoCalEntry
+    fun get(key: String): LoCalEntry?
 
     @Query("DELETE FROM $DATABASE_TABLE_NAME")
     fun clear()
