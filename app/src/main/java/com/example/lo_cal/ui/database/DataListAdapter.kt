39d81ddd327305a869231573833a5b5bf2ca773a
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-21 14:32:23 +0530

    Create adapter and view holder for recycler view

--- app/src/main/java/com/example/lo_cal/ui/database/DataListAdapter.kt
+++ app/src/main/java/com/example/lo_cal/ui/database/DataListAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.lo_cal.ui.database
 
-class DataListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lo_cal.R
+import com.example.lo_cal.data.models.LoCalEntry
+
+class DataListAdapter : RecyclerView.Adapter<DataListViewHolder>() {
+
+    var data = listOf<LoCalEntry>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataListViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater
+            .inflate(
+                R.layout.datalist_item,
+                parent, false
+            )
+        return DataListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: DataListViewHolder, position: Int) {
+        val dataItem = data[position]
+        holder.id.text = dataItem.id.toString()
+        holder.firstName.text = dataItem.firstName
+        holder.secondName.text = dataItem.secondName
+        holder.result.text = dataItem.result
+    }
+}
+
+class DataListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val id: TextView = itemView.findViewById(R.id.item_id)
+    val firstName: TextView = itemView.findViewById(R.id.item_first_name)
+    val secondName: TextView = itemView.findViewById(R.id.item_second_name)
+    val result: TextView = itemView.findViewById(R.id.item_result)
 }
