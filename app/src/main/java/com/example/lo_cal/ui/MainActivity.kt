4a2e0e8c5e297ec624e54f513731e7334fc9cf7a
shashank mohabia <shashankmohabia27@gmail.com>
2020-03-28 12:26:29 +0530

    Add navigation drawer to main activity the right way

--- app/src/main/java/com/example/lo_cal/ui/MainActivity.kt
+++ app/src/main/java/com/example/lo_cal/ui/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.lo_cal.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lo_cal.R
+import com.example.lo_cal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy {
+        this.findNavController(R.id.myNavHostFragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        binding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, binding.drawerLayout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
     }
 }
